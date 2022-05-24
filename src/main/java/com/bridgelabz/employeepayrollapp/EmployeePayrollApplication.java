fc193161d6971b9fb43cff27b8c22c2375d487ad
pragnakasthruri <kpragna25@gmail.com>
2022-05-24 11:54:14 +0530

    Adding the code for creating application.properties for dev and production

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -3,13 +3,15 @@ package com.bridgelabz.employeepayrollapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollApplication.class, args);
-        log.info("Employee Payroll App started");
+        ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+        log.info("Employee Payroll App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
     }
 }
