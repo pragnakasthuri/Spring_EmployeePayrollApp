07888c0fbcc50bc5fa375ba67024631837329ae0
pragnakasthruri <kpragna25@gmail.com>
2022-05-24 18:05:25 +0530

    Adding the code to add remaining properties to payroll DTO and Model

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,10 +1,13 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import lombok.ToString;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import java.util.List;
 
-public class EmployeePayrollDTO {
+public @ToString class EmployeePayrollDTO {
 
     @Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$", message="Employee name Invalid")
     @NotEmpty(message = "Employee Name can't be null")
@@ -12,15 +15,16 @@ public class EmployeePayrollDTO {
 
     @Min(value = 500, message = "Min Wage should be more than 500")
     public long salary;
-    public EmployeePayrollDTO(String name, long salary) {
-        super();
-        this.name = name;
-        this.salary = salary;
-    }
 
-    @Override
-    public String toString() {
-        return "EmployeePayrollDTO [name=" + name + ", salary=" + salary + "]";
-    }
+    public String gender;
+
+    public String startDate;
+
+    public String note;
+
+    public String profilePic;
+
+    public List<String> department;
+
 }
 
--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -3,11 +3,18 @@ package com.bridgelabz.employeepayrollapp.model;
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class EmployeePayrollData {
     private int employeeId;
     private String name;
     private long salary;
+    public String gender;
+    public String startDate;
+    public String note;
+    public String profilePic;
+    public List<String> department;
 
     public EmployeePayrollData() {
     }
@@ -17,5 +24,9 @@ public class EmployeePayrollData {
         this.employeeId = employeeId;
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
+        this.startDate = employeePayrollDTO.startDate;
+        this.note = employeePayrollDTO.note;
+        this.profilePic = employeePayrollDTO.profilePic;
+        this.department = employeePayrollDTO.department;
     }
 }
