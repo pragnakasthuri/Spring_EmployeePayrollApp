523b24ff5d43f58f882369eddbdce0e1a27aea66
pragnakasthruri <kpragna25@gmail.com>
2022-05-25 15:47:05 +0530

    Adding the code to to retrieve all the records of employee payroll pertaining to sales department

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -5,7 +5,6 @@ import lombok.ToString;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 public @ToString class EmployeePayrollDTO {
--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -5,7 +5,6 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Entity
